b0b41de8922f1d3b25221092b1893fd6d2a639de
Diaplo <adonahu1@nd.edu>
2015-03-19 12:04:05 -0400

    Further development and testing of the DG to CG projection method.
    It appears to work well now, however the solution set is still not "smooth" enough
    to produce adequately smooth derivatives for use in the Pressure Poisson model.
    
    Two strategies have been proposed:
    
    1)  Consider techniques for post-processing the projected DG solutions so as to smooth them
        sufficiently for "clean" derivatives.  -Jessica will look at this
    
    2)  Switch to a CG implementation of the Pressure Poisson model. - Aaron will look at this

--- work/check_pp.m
+++ work/check_pp.m
@@ -5,11 +5,44 @@ for k = 1:length(etaver1(:,1))
     eta2(etaver2(k,1),etaver2(k,2)) = etaver2(k,3);
 end
 
+% etalng = [eta1(:,1) eta1 eta1(:,end)];
+% for k = 1:length(eta1(:,1))
+%     for iter = 1:20
+%         etalng(k,:) = smooth(etalng(k,:))';
+%     end
+% end
+
+% eta1 = etalng(:,2:end-1);
+etalng = [eta1(:,1) eta1 eta1(:,end)];
+eta1x = (etalng(:,3:end)-etalng(:,1:end-2));
+% eta1x = eta1(:,2:end)-eta1(:,1:end-1);
+etalng = [eta2(:,1) eta2 eta2(:,end)];
+eta2x = (etalng(:,3:end)-etalng(:,1:end-2));
+% eta2x = eta2(:,2:end)-eta2(:,1:end-1);
+
+
 figure(1)
 subplot(1,2,1)
 surf(eta1)
 subplot(1,2,2)
 surf(eta2)
+
+figure(2)
+subplot(1,2,1)
+surf(eta1x)
+subplot(1,2,2)
+surf(eta2x)
+
+etalng = [eta1(:,1) eta1 eta1(:,end)];
+eta1xx = (etalng(:,3:end)-2*etalng(:,2:end-1)+etalng(:,1:end-2));
+etalng = [eta2(:,1) eta2 eta2(:,end)];
+eta2xx = (etalng(:,3:end)-2*etalng(:,2:end-1)+etalng(:,1:end-2));
+
+figure(3)
+subplot(1,2,1)
+surf(eta1xx)
+subplot(1,2,2)
+surf(eta2xx)
 
 return
 %%
