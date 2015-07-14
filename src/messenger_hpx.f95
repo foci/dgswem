61f227590dbf895db942f915b8257277ecc727e6
Steven Brus <sbrus@nd.edu>
2015-07-14 17:40:02 -0400

    Add fortran boundary exchange rountines
     - HPX_GET_ELEMS puts a domain's solution data to be exchenged with
       neighbor into a sendbuf vector
    
     - HPX_PUT_ELEMS takes a solution from a domain's neighbor in recvbuf
       and puts it into its solution arrays.

--- src/messenger_hpx.f95
+++ src/messenger_hpx.f95
@@ -1,7 +1,94 @@
 #ifdef HPX
-subroutine HPX_GET_ELEMS()
-end subroutine HPX_GET_ELEMS
+       subroutine HPX_GET_ELEMS(dg_here,neighbor,volume,sendbuf)     
+       
+       use global
+       use dg
+       use sizes
+       
+       implicit none       
 
-subroutine HPX_PUT_ELEMS()
-end subroutine HPX_PUT_ELEMS
+       type (sizes_type) :: s
+       type (dg_type) :: dg_here
+       type (global_type) :: global_here
+       
+       integer :: el,k
+       integer :: ncount
+       
+       real(sz) :: sendbuf(volume)
+       
+      
+       ncount = 0
+       DO el=1,dg_here%NELEMSEND(neighbor)
+          DO dof=1,dg_here%DOFH
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%ZE(dof,dg_here%ISENDLOC(el,neighbor),dg_here%IRK)
+          ENDDO
+       ENDDO
+       
+       DO el=1,dg_here%NELEMSEND(neighbor)
+         DO dof=1,dg_here%DOFH
+           ncount = ncount+1
+           sendbuf(ncount)=dg_here%QX(dof,dg_here%ISENDLOC(el,neighbor),dg_here%IRK)
+         ENDDO
+       ENDDO
+          
+       DO el=1,dg_here%NELEMSEND(neighbor)
+         DO dof=1,dg_here%DOFH
+           ncount = ncount+1
+           sendbuf(ncount)=dg_here%QY(dof,dg_here%ISENDLOC(el,neighbor),dg_here%IRK)
+         ENDDO
+       ENDDO
+    
+       
+       
+       end subroutine HPX_GET_ELEMS
+       
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!           
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!       
+
+       subroutine HPX_PUT_ELEMS(dg_here,neighbor,volume,recvbuf)
+       
+       use global
+       use dg
+       use sizes
+       
+       implicit none
+       
+       type (dg_type) :: dg_here
+       
+       integer :: i,k
+       integer :: ncount
+       integer :: neighbor
+       integer :: volume
+       
+       real(sz) :: recvbuf(volume)      
+       
+             
+       
+       ncount = 0
+       DO el=1,dg_here%NELEMRECV(neighbor)
+         DO dof=1,dg_here%DOFH
+           ncount = ncount+1
+           dg_here%ZE(dof,dg_here%IRECVLOC(el,neighbor),dg_here%IRK) = recvbuf(ncount)
+         ENDDO
+       ENDDO
+       
+       DO el=1,dg_here%NELEMRECV(neighbor)
+         DO dof=1,dg_here%DOFH
+           ncount = ncount+1
+           dg_here%QX(dof,dg_here%IRECVLOC(el,neighbor),dg_here%IRK) = recvbuf(ncount)
+         ENDDO
+       ENDDO
+       
+       DO el=1,dg_here%NELEMRECV(neighbor)
+         DO dof=1,dg_here%DOFH
+           ncount = ncount+1
+           dg_here%QY(dof,dg_here%IRECVLOC(el,neighbor),dg_here%IRK) = recvbuf(ncount)
+         ENDDO
+       ENDDO         
+         
+
+     
+       
+       end subroutine HPX_PUT_ELEMS
 #endif
