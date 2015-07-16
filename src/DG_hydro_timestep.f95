6b01336eed6e8c5284b48f7beebf4b1897dbe826
Steven Brus <sbrus@nd.edu>
2015-07-16 17:30:15 -0400

    Modifications to DG_HYDRO_TIMESTEP
     - Remove the outer RK stage loop and add RK stage as subroutine
       argument
    
     - Use if statements to separate out computing the current time
       (only before first stage) and preparing for next time step
       (only after last stage)
    
     - Include the last 3 calls of DG_TIMESTEP so they occur after
       the last RK stage, now DG_HYDRO_TIMESTEP can be called directly.
    
       ** DG_TIMESTEP shouldn't have to be called anymore,
          but DG_timestep.f95 needs to be compiled since it contains
          the SCRUTINIZE_SOLUTION subroutine.

--- src/DG_hydro_timestep.f95
+++ src/DG_hydro_timestep.f95
@@ -21,7 +21,7 @@
 !     
 !***********************************************************************
 
-      SUBROUTINE DG_HYDRO_TIMESTEP(s,dg_here,global_here,nodalattr_here,IT)
+      SUBROUTINE DG_HYDRO_TIMESTEP(s,dg_here,global_here,nodalattr_here,IT,IRK)
 
 !.....Use appropriate modules
       USE SIZES
@@ -58,22 +58,26 @@
       Real(SZ),allocatable :: XB(:),YB(:),radial(:)
 
       Allocate ( XB(S%MNE),YB(S%MNE),radial(S%MNE) )
+      
+      IF (IRK == 1) THEN      
 
-!.....Compute the current time
+!.......Compute the current time
 
-      ITDT = IT*global_here%DTDP
-      global_here%TIME_A = ITDT + global_here%STATIM*86400.D0
-      global_here%TIMEH  = ITDT + (global_here%STATIM - global_here%REFTIM)*86400.D0
+        ITDT = IT*global_here%DTDP
+        global_here%TIME_A = ITDT + global_here%STATIM*86400.D0
+        global_here%TIMEH  = ITDT + (global_here%STATIM - global_here%REFTIM)*86400.D0
 
-!.....Update global_here%ETA1 (shintaro: do we need this?)
+!.......Update global_here%ETA1 (shintaro: do we need this?)
 
-      DO I = 1,S%MNP
-         global_here%ETA1(I) = global_here%ETA2(I)
-      ENDDO
+        DO I = 1,S%MNP
+           global_here%ETA1(I) = global_here%ETA2(I)
+        ENDDO
+      
+      ENDIF
 
 !.....Begin Runge-Kutta time stepper
 
-      DO 100 irk = 1,dg_here%NRK
+!       DO 100 irk = 1,dg_here%NRK
          dg_here%irk = irk
 !.......Compute the DG time and DG global_here%ramp
 
@@ -554,133 +558,159 @@
 
 #endif
 
- 100  CONTINUE
+!  100  CONTINUE
 
-!.....Update variables for next time step
+      IF (IRK == dg_here%NRK) THEN
+
+!.......Update variables for next time step
 
 #ifdef FILTER 
-      !Stabilization filtering 
-      DO J = 1,global_here%NE
-         DO K = 2,dg_here%DOFS(J)
+        !Stabilization filtering 
+        DO J = 1,global_here%NE
+           DO K = 2,dg_here%DOFS(J)
 
-            alph = 10.D0
-            s_dg = dg_here%RK_stage
+              alph = 10.D0
+              s_dg = dg_here%RK_stage
 
-            if (k.le.3) then
-               eta = 1.D0 / ( global_here%pdg_el(j)+1.D0 )
-            elseif (k.gt.3.and.k.le.6) then
-               eta = 2.D0 / ( global_here%pdg_el(j)+1.D0 )
-            elseif (k.gt.6.and.k.le.10) then
-               eta = 3.D0 / ( global_here%pdg_el(j)+1.D0 )
-            elseif (k.gt.11.and.k.le.15) then
-               eta = 4.D0 / ( global_here%pdg_el(j)+1.D0 )
-            elseif (k.gt.16.and.k.le.21) then
-               eta = 5.D0 / ( global_here%pdg_el(j)+1.D0 )       
-            elseif (k.gt.22.and.k.le.28) then
-               eta = 6.D0 / ( global_here%pdg_el(j)+1.D0 )   
-            elseif (k.gt.29.and.k.le.36) then
-               eta = 7.D0 / ( global_here%pdg_el(j)+1.D0 )               
-            endif
+              if (k.le.3) then
+                 eta = 1.D0 / ( global_here%pdg_el(j)+1.D0 )
+              elseif (k.gt.3.and.k.le.6) then
+                 eta = 2.D0 / ( global_here%pdg_el(j)+1.D0 )
+              elseif (k.gt.6.and.k.le.10) then
+                 eta = 3.D0 / ( global_here%pdg_el(j)+1.D0 )
+              elseif (k.gt.11.and.k.le.15) then
+                 eta = 4.D0 / ( global_here%pdg_el(j)+1.D0 )
+              elseif (k.gt.16.and.k.le.21) then
+                 eta = 5.D0 / ( global_here%pdg_el(j)+1.D0 )       
+              elseif (k.gt.22.and.k.le.28) then
+                 eta = 6.D0 / ( global_here%pdg_el(j)+1.D0 )   
+              elseif (k.gt.29.and.k.le.36) then
+                 eta = 7.D0 / ( global_here%pdg_el(j)+1.D0 )               
+              endif
 
-            seta = eta**s_dg
-            sigma = exp(-alph*seta)
+              seta = eta**s_dg
+              sigma = exp(-alph*seta)
 
-!.......Filter the layers if flagged
+!.........Filter the layers if flagged
 
 #ifdef SED_LAY
-            do l = 1,layers
+              do l = 1,layers
+                
+                 dg_here%bed(K,J,dg_here%NRK+1,l) = sigma * dg_here%bed(K,J,dg_here%nrk+1,l) 
                
-               dg_here%bed(K,J,dg_here%NRK+1,l) = sigma * dg_here%bed(K,J,dg_here%nrk+1,l) 
-               
-            enddo
+              enddo
 #endif
 
-            dg_here%ZE(K,J,dg_here%NRK+1) = sigma*dg_here%ZE(K,J,dg_here%nrk+1) 
-            dg_here%QX(K,J,dg_here%NRK+1) = sigma*dg_here%QX(K,J,dg_here%nrk+1)
-            dg_here%QY(K,J,dg_here%NRK+1) = sigma*dg_here%QY(K,J,dg_here%nrk+1)
+              dg_here%ZE(K,J,dg_here%NRK+1) = sigma*dg_here%ZE(K,J,dg_here%nrk+1) 
+              dg_here%QX(K,J,dg_here%NRK+1) = sigma*dg_here%QX(K,J,dg_here%nrk+1)
+              dg_here%QY(K,J,dg_here%NRK+1) = sigma*dg_here%QY(K,J,dg_here%nrk+1)
 
 
-!.......Filter the transported global_here%tracer term if flagged
+!.........Filter the transported global_here%tracer term if flagged
 
 #ifdef TRACE
-            dg_here%iota(K,J,dg_here%NRK+1) = sigma*dg_here%iota(K,J,dg_here%nrk+1)
+              dg_here%iota(K,J,dg_here%NRK+1) = sigma*dg_here%iota(K,J,dg_here%nrk+1)
 
 #endif
 
-!......Filter chemistry transported terms if flagged
+!........Filter chemistry transported terms if flagged
 
 #ifdef CHEM
-            dg_here%iota(K,J,dg_here%NRK+1) =  sigma*dg_here%iota(K,J,dg_here%nrk+1) 
-            dg_here%iota2(K,J,dg_here%NRK+1) = sigma*dg_here%iota2(K,J,dg_here%nrk+1) 
+              dg_here%iota(K,J,dg_here%NRK+1) =  sigma*dg_here%iota(K,J,dg_here%nrk+1) 
+              dg_here%iota2(K,J,dg_here%NRK+1) = sigma*dg_here%iota2(K,J,dg_here%nrk+1) 
 
 #endif
 
-         ENDDO
-      ENDDO
+           ENDDO
+        ENDDO
 #endif
       
 
-      DO J = 1,S%MNE
-         
-         DO K = 1,dg_here%DOFS(J)
-            dg_here%ZE(K,J,1) = dg_here%ZE(K,J,dg_here%NRK+1)
-            dg_here%QX(K,J,1) = dg_here%QX(K,J,dg_here%NRK+1)
-            dg_here%QY(K,J,1) = dg_here%QY(K,J,dg_here%NRK+1)
+        DO J = 1,S%MNE
+           
+           DO K = 1,dg_here%DOFS(J)
+              dg_here%ZE(K,J,1) = dg_here%ZE(K,J,dg_here%NRK+1)
+              dg_here%QX(K,J,1) = dg_here%QX(K,J,dg_here%NRK+1)
+              dg_here%QY(K,J,1) = dg_here%QY(K,J,dg_here%NRK+1)
 #ifdef TRACE
-            dg_here%iota(K,J,1) = dg_here%iota(K,J,dg_here%NRK+1)
+              dg_here%iota(K,J,1) = dg_here%iota(K,J,dg_here%NRK+1)
 #endif
 
 #ifdef CHEM
-            dg_here%iota(K,J,1) = dg_here%iota(K,J,dg_here%NRK+1)
-            dg_here%iota2(K,J,1) = dg_here%iota2(K,J,dg_here%NRK+1)
+              dg_here%iota(K,J,1) = dg_here%iota(K,J,dg_here%NRK+1)
+              dg_here%iota2(K,J,1) = dg_here%iota2(K,J,dg_here%NRK+1)
 #endif
 
 #ifdef DYNP
-            dg_here%dynP(K,J,1) = dg_here%dynP(K,J,dg_here%NRK+1)
+              dg_here%dynP(K,J,1) = dg_here%dynP(K,J,dg_here%NRK+1)
 #endif
 
 #ifdef SED_LAY
-            dg_here%bed(K,J,1,:) = dg_here%bed(K,J,dg_here%NRK+1,:)
+              dg_here%bed(K,J,1,:) = dg_here%bed(K,J,dg_here%NRK+1,:)
 #endif
-         ENDDO
-      ENDDO
+           ENDDO
+        ENDDO
 
-      DO KK = 2,dg_here%NRK+1
-         DO J = 1,S%MNE
-            DO K = 1,dg_here%DOFH
-               dg_here%ZE(K,J,KK) = 0.D0
-               dg_here%QX(K,J,KK) = 0.D0
-               dg_here%QY(K,J,KK) = 0.D0
-               dg_here%RHS_ZE(K,J,KK-1) = 0.D0
-               dg_here%RHS_QX(K,J,KK-1) = 0.D0
-               dg_here%RHS_QY(K,J,KK-1) = 0.D0
+        DO KK = 2,dg_here%NRK+1
+           DO J = 1,S%MNE
+              DO K = 1,dg_here%DOFH
+                 dg_here%ZE(K,J,KK) = 0.D0
+                 dg_here%QX(K,J,KK) = 0.D0
+                 dg_here%QY(K,J,KK) = 0.D0
+                 dg_here%RHS_ZE(K,J,KK-1) = 0.D0
+                 dg_here%RHS_QX(K,J,KK-1) = 0.D0
+                 dg_here%RHS_QY(K,J,KK-1) = 0.D0
 #ifdef TRACE
-               dg_here%iota(K,J,KK) = 0.D0
-               dg_here%RHS_iota(K,J,KK-1) = 0.D0
+                 dg_here%iota(K,J,KK) = 0.D0
+                 dg_here%RHS_iota(K,J,KK-1) = 0.D0
 #endif
 
 #ifdef CHEM
-               dg_here%iota(K,J,KK) = 0.D0
-               dg_here%iota2(K,J,KK) = 0.D0
-               dg_here%RHS_iota(K,J,KK-1) = 0.D0
-               dg_here%RHS_iota2(K,J,KK-1) = 0.D0
+                 dg_here%iota(K,J,KK) = 0.D0
+                 dg_here%iota2(K,J,KK) = 0.D0
+                 dg_here%RHS_iota(K,J,KK-1) = 0.D0
+                 dg_here%RHS_iota2(K,J,KK-1) = 0.D0
 #endif
 
 #ifdef DYNP
-               dg_here%dynP(K,J,KK) = 0.D0
-               dg_here%RHS_dynP(K,J,KK-1) = 0.D0
+                 dg_here%dynP(K,J,KK) = 0.D0
+                 dg_here%RHS_dynP(K,J,KK-1) = 0.D0
 #endif
 
 #ifdef SED_LAY
-               dg_here%bed(K,J,KK,:) = 0.D0
-               dg_here%hb(K,J,KK) = 0.D0
-               dg_here%RHS_bed(K,J,KK-1,:) = 0.D0
+                 dg_here%bed(K,J,KK,:) = 0.D0
+                 dg_here%hb(K,J,KK) = 0.D0
+                 dg_here%RHS_bed(K,J,KK-1,:) = 0.D0
 #endif
 
 
-            ENDDO
-         ENDDO
-      ENDDO
+              ENDDO
+           ENDDO
+        ENDDO
+        
+#ifdef CMPI
+#ifdef BLKOUT
+        CALL MSG_BLOCKSYNC_START()
+#endif
+#endif
+
+        CALL WRITE_RESULTS(s,dg_here,global_here,IT,.FALSE.)
+        
+#ifdef CMPI
+#ifdef BLKOUT
+        CALL MSG_BLOCKSYNC_FINISH()
+#endif
+#endif
+
+        CALL SCRUTINIZE_SOLUTION(s,dg_here,global_here,IT)        
+      
+      
+      ENDIF
 
       RETURN
       END SUBROUTINE
+      
+      
+      
+      
+      
