3f62f49b01311fc0fd396cb2bf7ddc1f18a94816
Zach Byerly <zbyerly@gmail.com>
2015-08-04 12:38:34 -0500

    adding dgswem_lgd

--- src/dgswem_lgd.cpp
+++ src/dgswem_lgd.cpp
@@ -4,19 +4,16 @@
 #include <libgeodecomp.h>
 #include <libgeodecomp/geometry/unstructuredgridmesher.h>
 #include <libgeodecomp/io/logger.h>
+
 #include "fname.h"
-
-#include "fortran_includes.hpp"
-
+#include "fortran_declarations.hpp"
 
 std::vector<int> neighboringDomainIDs(void *size, void *dg, void *global)
 {
 
-    //std::cout << "twerk1\n";
     int numneighbors_fort;
     int neighbors_fort[MAX_DOMAIN_NEIGHBORS];
-    //std::cout << "twerk2 " << domain << "\n";
-    if (!domain) {
+    if (!size) {
         return std::vector<int>();
     }
 
@@ -25,11 +22,8 @@ std::vector<int> neighboringDomainIDs(void *size, void *dg, void *global)
 			      &global,
 			      neighbors_fort,
 			      &numneighbors_fort);
-    //std::cout << "twerk3\n";
     std::vector<int> ret(numneighbors_fort);
-    //std::cout << "twerk4\n";
     std::copy(neighbors_fort, neighbors_fort + numneighbors_fort, ret.begin());
-    //std::cout << "twerk5\n";
 
     return ret;
 }
@@ -68,7 +62,7 @@ public:
     explicit
     DomainReference(int id = 0, void *size = 0, void *global = 0, void *dg = 0, void *nodalattr = 0) :
         domainWrapper(new FortranPointerWrapper(size, global, dg, nodalattr)),
-        neighbors_here(neighboringDomainIDs(domain)),
+        neighbors_here(neighboringDomainIDs(size,dg,global)),
         id(id),
         timestep(0)
     {}
@@ -77,9 +71,7 @@ public:
     void update(const HOOD& hood, int nanoStep)
     {
         std::cout << "updating...\n";
-        // Update values using Game of Life rules
-        //FNAME(update_fort)(&timestep, &domainWrapper->domain);
-	FNAME(dg_timestep_fort)(&domainWrapper->size,
+	FNAME(dg_hydro_timestep_fort)(&domainWrapper->size,
 				&domainWrapper->dg,
 				&domainWrapper->global,
 				&domainWrapper->nodalattr,
@@ -130,7 +122,7 @@ class FortranInitializer : public LibGeoDecomp::SimpleInitializer<FortranCell>
 public:
     using SimpleInitializer<FortranCell>::dimensions;
 
-    FortranInitializer(const std::string& base_path, std::size_t numDomains, std::size_t numSteps) :
+    FortranInitializer(std::size_t numDomains, std::size_t numSteps) :
         SimpleInitializer<FortranCell>(LibGeoDecomp::Coord<2>(), numSteps),
         numDomains(numDomains)
     {
@@ -152,7 +144,7 @@ public:
 
             FNAME(term_fort)(&domain);
 	*/
-
+	    neighbors.push_back(0);
             LibGeoDecomp::FloatCoord<2> coord;
 	    coord[0] = 0.0;
 	    domainCoords << coord;
@@ -202,108 +194,13 @@ private:
 
 int main(int argc, char* argv[])
 {
-    // std::vector<int> ids;
-
-    // std::vector<int> numneighbors;
-    // std::vector<std::vector<int> > neighbors;
-
-    // fixme: make these configurable via command line
+    // fixme: these should be read in via config files
     int n_domains = 32;
     int n_timesteps = 100;
 
-    // parse command line
-    if (argc != 2) {
-        std::cerr << "USAGE: " << argv[0] << " PATH_TO_DECOMPOSE_MESH" << std::endl;
-        return 1;
-    }
-
     LibGeoDecomp::SerialSimulator<FortranCell> sim(
-        new FortranInitializer(argv[1], n_domains, n_timesteps));
+        new FortranInitializer(n_domains, n_timesteps));
     sim.run();
-
-    // // Get list of neighbors from FORTRAN side of each domain
-    // for(int i=0; i<domains.size(); i++) {
-    //     int numneighbors_fort;
-    //     int neighbors_fort[MAX_DOMAIN_NEIGHBORS];
-    //     std::vector<int> neighbors_here;
-
-    //     FNAME(get_neighbors_fort)(&numneighbors_fort,neighbors_fort,&domains[i]);
-    //     numneighbors.push_back(numneighbors_fort);
-
-    //     for (int j=0; j<numneighbors_fort; j++) {
-    //         std::cout << neighbors_fort[j] << " ";
-    //         neighbors_here.push_back(neighbors_fort[j]);
-    //     }
-    //     std::cout << std::endl;
-
-    //     neighbors.push_back(neighbors_here);
-
-    // }
-
-    // // Print out some information about the domains and their neighbors
-    // std::cout << "*** Grid Information ***" << std::endl;
-    // std::cout << "domains.size() = " << domains.size() << std::endl;
-    // std::cout << "numneighbors.size() = " << numneighbors.size() << std::endl;
-    // for (int domain=0; domain<numneighbors.size(); domain++) {
-    //     std::cout << "numneighbors[" << domain << "] = " << numneighbors[domain] << std::endl;
-    //     std::cout << "neighbors: ";
-    //     std::vector<int> neighbors_here = neighbors[domain];
-    //     for (int neighbor=0; neighbor<numneighbors[domain]; neighbor++) {
-    //         std::cout << neighbors_here[neighbor] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    // std::cout << "*** End Grid Information ***" << std::endl;
-
-    // // Print initial state as timestep 0
-    // int zero = 0;
-    // for(int i=0; i<domains.size(); i++) {
-    //     FNAME(print_fort)(&domains[i], &zero);
-    // }
-
-
-    // // Start timestepping loop
-    // for(int timestep=1; timestep<=n_timesteps; timestep++) {
-
-    //     // Update values using Game of Life rules
-    //     // Loop over all domains
-    //     for (int j=0; j<domains.size(); j++) {
-    //         FNAME(update_fort)(&timestep,&domains[j]);
-    //     } // End loop over domains
-
-    //     // Boundary exchange
-    //     // Loop over domains
-    //     for (int domain=0; domain<domains.size(); domain++) {
-    //         std::vector<int> neighbors_here = neighbors[domain];
-
-    //         //Loop over neighbors
-    //         for (int neighbor=0; neighbor<numneighbors[domain]; neighbor++) {
-    //             int neighbor_here = neighbors_here[neighbor];
-    //             int num_nodes;
-    //             int alive[MAX_BOUNDARY_SIZE];
-
-    //             // Get outgoing boundarys from the neighbors
-    //             FNAME(get_outgoing_nodes_fort)(&domains[neighbor_here],&domain,&num_nodes,alive);
-
-    //             // Put those arrays inside current domain
-    //             FNAME(put_incoming_nodes_fort)(&domains[domain],&neighbor_here,&num_nodes,alive);
-
-    //         }// end loop over neighbors
-
-    //     }// end loop over domains
-
-    //     // Print domains
-    //     for(int i=0; i<domains.size(); i++) {
-    //         FNAME(print_fort)(&domains[i], &timestep);
-    //     }
-
-    // } // end timestep loop
-
-    // // Deallocate domains
-    // for(int i=0; i<domains.size(); i++) {
-    //     FNAME(term_fort)(&domains[i]);
-    // }
 
     return 0;
 }
