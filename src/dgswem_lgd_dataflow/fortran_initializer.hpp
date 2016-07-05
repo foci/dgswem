0e5ba4514fc890a81686e53fafd0cd4cabf0e907
Zach Byerly <zbyerly@cct.lsu.edu>
2016-07-05 17:46:52 -0500

    correcting some problems with CMakeLists

--- src/dgswem_lgd_dataflow/fortran_initializer.hpp
+++ src/dgswem_lgd_dataflow/fortran_initializer.hpp
@@ -25,6 +25,8 @@ public:
     {
 	boost::shared_ptr<LibGeoDecomp::Adjacency> adjacency(new LibGeoDecomp::RegionBasedAdjacency());
 
+	hpx::cout << "calling Adjacency" << std::endl;
+
         for(int id = 0; id < numDomains ; id++) {
 	    /*
 	    void *size = NULL;
@@ -55,7 +57,7 @@ public:
 	    std::ostringstream filenamess;
 	    filenamess << std::setw(4) << std::setfill('0') << id;
 	    std::string filename = "PE"+filenamess.str()+"/DG.18";
-	    std::cout << "filename = " << filename << std::endl;
+	    //std::cout << "filename = " << filename << std::endl;
 	    dg18file.open(filename);
 	    std::string line;
 	    int line_num = 1;
