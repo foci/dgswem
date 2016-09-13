21156dded4733a51155d81e3f3be85e7918f487f
Steven Brus <sbrus@nd.edu>
2016-09-13 17:09:15 -0400

    Add subroutines to get and put nodal min/maxes
     - hpx_get_nodes and hpx_put_nodes are in messenger_hpx.F90

--- src/messenger_hpx.F90
+++ src/messenger_hpx.F90
@@ -187,25 +187,189 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!           
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
 
-      subroutine HPX_GET_NODES()
+      subroutine HPX_GET_NODES(dg_here,domain,volume,sendbuf)
 
       implicit none
 
+       
+       use dg
+       use sizes
+       
+       implicit none       
 
+       type (dg_type) :: dg_here
+       
+       logical :: neighbor_found
+       integer :: i,index
+       integer :: nd
+       integer :: ncount
+       integer :: domain
+       integer :: volume
+       
+!       real(sz) :: sendbuf(volume)
+       real(sz) :: sendbuf(MAX_BUFFER_SIZE)   
+
+       
+       neighbor_found = .false.
+       DO i=1,dg_here%NEIGHPROC
+
+         IF (dg_here%IPROC(i) == domain) THEN
+           index = i
+           neighbor_found = .true.
+           EXIT
+         ENDIF
+      
+       ENDDO     
+       
+       IF (neighbor_found) THEN
+       
+!          PRINT*, " PROC: ", dg_here%IDPROC, " SENDING ", dg_here%NNODESEND(index) ," NODES TO: ", dg_here%IPROC(index)       
+!          PRINT 180, (dg_here%ISENDLOC_NODE(nd,index), nd = 1,dg_here%NNODESEND(index))
+!          PRINT*, "" 
+      
+         ncount = 0
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%ZE_MIN1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%ZE_MAX1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%QX_MIN1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%QX_MAX1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%QY_MIN1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+
+         DO nd=1,dg_here%NNODESEND(index)
+            ncount = ncount+1
+            sendbuf(ncount)=dg_here%QY_MAX1(dg_here%ISENDLOC_NODE(nd,index))
+         ENDDO
+       
+         
+         volume = ncount
+
+!          PRINT*, "----------------------------------------------------------------------------------"                  
+       
+       ELSE 
+       
+!          PRINT*, "FORTRAN ERROR: send neighbor not found"   
+!          PRINT*, "SEND PROC: ", dg_here%IDPROC, " DOMAIN: ", domain                 
+!          STOP             
+    
+       ENDIF
+       
+ 180  FORMAT(8X,9I8)    
+ 181  FORMAT(I5,2X,ES24.17)
 
       return
-      end subroutine HPX_GET_NODES()
+      end subroutine HPX_GET_NODES
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!           
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 
-       subroutine HPX_PUT_NODES()
+       subroutine HPX_PUT_NODES(dg_here,neighbor,volume,recvbuf)
 
        implicit none
 
+       use dg
+       use sizes
+       
+       implicit none
+       
+       type (dg_type) :: dg_here
+       
+       logical :: neighbor_found
+       integer :: i,index       
+       integer :: nd
+       integer :: ncount
+       integer :: neighbor
+       integer :: volume
+       
+!       real(sz) :: recvbuf(volume)
+       real(sz) :: recvbuf(MAX_BUFFER_SIZE)
+        
+
+       neighbor_found = .false.
+       DO i=1,dg_here%NEIGHPROC
+
+         IF (dg_here%IPROC(i) == neighbor) THEN
+           index = i
+           neighbor_found = .true.
+           EXIT
+         ENDIF
+      
+       ENDDO           
+             
+       IF (neighbor_found) THEN  
+       
+!          PRINT*, " PROC: ", dg_here%IDPROC, " RECEIVING ",dg_here%NNODERECV(index), " NODES FROM: ", dg_here%IPROC(index)
+!          PRINT 180, (dg_here%IRECVLOC_NODE(el,index), el = 1,dg_here%NNODERECV(index))
+!          PRINT*, ""
+         
+         ncount = 0
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%ZE_MIN1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%ZE_MAX1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+         ncount = 0
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%QX_MIN1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%QX_MAX1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+         ncount = 0
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%QY_MIN1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+         DO nd=1,dg_here%NNODERECV(index)
+           ncount = ncount+1
+           dg_here%QY_MAX1(dg_here%IRECVLOC_NODE(nd,index)) = recvbuf(ncount)
+         ENDDO
+
+       
+        
+!          PRINT*, "----------------------------------------------------------------------------------"         
+       
+       ELSE
+       
+!          PRINT*, "FORTRAN ERROR: recieve neighbor not found"
+!          PRINT*, "RECV PROC: ", dg_here%IDPROC, " NEIGHBOR: ", neighbor 
+!          STOP       
+         
+       ENDIF
+         
+
+ 180  FORMAT(8X,9I8)     
+ 181  FORMAT(I5,2X,ES24.17) 
 
        return
-       end subroutine HPX_PUT_NODES()
+       end subroutine HPX_PUT_NODES
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!           
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!      
