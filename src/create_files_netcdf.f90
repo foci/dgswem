2fa7ca53b0048362e6a8c0e3b00db0bdd7db2597
ashtonvcole <asthoncole1028@gmail.com>
2026-03-26 13:59:25 -0500

    Working on creating fort.63 file

--- src/create_files_netcdf.f90
+++ src/create_files_netcdf.f90
@@ -28,44 +28,70 @@ module create_files_netcdf
         implicit none
         
         integer :: stat ! Status of each NetCDF function call
-        integer :: ncid ! ID of current NetCDF file being operated on
-        integer :: dimid ! ID of current dimension being operated on
-        integer :: varid ! ID of current variable being operated on
+        integer :: fort_63_ncid ! ID of fort.63.nc NetCDF file.
+        integer :: time_dimid ! ID of time dimension
+        integer :: node_dimid ! ID of node dimension
+        integer :: nele_dimid ! ID of nele dimension
+        integer :: nvertex_dimid ! ID of nvertex dimension
+        integer :: nope_dimid ! ID of nope dimension
+        integer :: neta_dimid ! ID of neta dimension
+        integer :: max_nvdll_dimid ! ID of max_nvdll dimension
+        integer :: nbou_dimid ! ID of nbou dimension
+        integer :: nvel_dimid ! ID of nvel dimension
+        integer :: max_nvell_dimid ! ID of max_nvell dimension
+        integer :: mesh_dimid ! ID of mesh dimension
+        integer :: time_varid ! ID of time variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
+        integer :: _varid ! ID of  variable
         
         ! fort.63.nc: elevation time series
-        stat = nf90_create('fort.63.nc', nf90_clobber, ncid)
+        stat = nf90_create('fort.63.nc', nf90_clobber, fort_63_ncid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "time", nf90_unlimited, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "time", nf90_unlimited, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "node", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "node", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "nele", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "nele", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "nvertex", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "nvertex", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "nope", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "nope", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "neta", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "neta", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "max_nvdll", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "max_nvdll", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "nbou", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "nbou", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "nvel", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "nvel", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "max_nvell", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "max_nvell", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_dim(ncid, "mesh", ???, dimid)
+        stat = nf90_def_dim(fort_63_ncid, "mesh", ???, dimid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_def_var(ncid, "time", nf90_double, &
+        stat = nf90_def_var(fort_63_ncid, "time", nf90_double, &
             (/  /), varid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
         stat = nf90_put_att
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_enddef(ncid)
+        stat = nf90_enddef(fort_63_ncid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-        stat = nf90_close(ncid)
+        stat = nf90_close(fort_63_ncid)
         if (stat.neq.nf90_noerr) call handle_err(stat)
-    end subroutine create_fort_63()
+    end subroutine create_fort_63
 
 end module create_files_netcdf
