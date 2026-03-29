38edcd17384dd35ac270efb882243ec4ee12e5a9
Ashton Cole <ashtoncole1028@gmail.com>
2026-03-29 18:04:44 -0500

    Initial skeleton for fort NetCDFs

---
+++ src/netcdf_fort.f90
@@ -0,0 +1,10 @@
+module netcdf_fort
+    !! Module for creating and writing to ADCIRC-style fort files.
+    
+    use netcdf
+    
+    implicit none
+    
+    contains
+        
+end module netcdf_fort
