663b792a73e7a21e4d93eb1c08d543d49fa1acc8
namo <namo26june@pm.me>
2026-03-31 19:12:26 -0500

    improve pytest

--- tests/util.py
+++ tests/util.py
@@ -40,13 +40,13 @@ def run_serial(binpath, testpath, rtol=0.05, atol=0.01):
     if not os.path.exists(os.path.join(binpath, "dgswem_serial")):
         raise FileNotFoundError('dgswem_serial not found')
 
-    try:
-        result = subprocess.run(os.path.join(binpath, "dgswem_serial"),
-                                check=True, cwd=testpath, shell=True,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print(e.stdout)
+    result = subprocess.run(os.path.join(binpath, "dgswem_serial"),
+                            check=False, cwd=testpath, shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    if result.returncode != 0:
+        print(result.stdout.decode())
+        sys.exit("DG-SWEM serial run failed. See output above.")
 
     d1, _ = last_snapshot(os.path.join(testpath , "fort.63.true"))
     d2, _ = last_snapshot(os.path.join(testpath , "fort.63"))
@@ -60,20 +60,21 @@ def run_parallel(binpath, testpath, rtol=0.05, atol=0.01):
     if not os.path.exists(os.path.join(binpath, "adcpost")):
         pytest.skip("adcpost executable not found. Skipping...")
 
-    try:
-        prep = subprocess.run(os.path.join(binpath, "adcprep") + " < in.prep", check=True, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print(e.stdout)
+    result = subprocess.run(os.path.join(binpath, "adcprep") + " < in.prep", check=False, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if result.returncode != 0:
+        print(result.stdout.decode())
+        sys.exit("ADCPREP failed. See output above.")
 
-    try:
-        result = subprocess.run("mpirun -np 2 " + os.path.join(binpath, "dgswem"), check=True, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print(e.stdout)
 
-    try:
-        post = subprocess.run(os.path.join(binpath, "adcpost") + " < out.prep", check=True, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print(e.stdout)
+    result = subprocess.run("mpirun -np 2 " + os.path.join(binpath, "dgswem"), check=False, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if result.returncode != 0:
+        print(result.stdout.decode())
+        sys.exit("DG-SWEM parallel run failed. See output above.")
+
+    result = subprocess.run(os.path.join(binpath, "adcpost") + " < out.prep", check=False, cwd=testpath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if result.returncode != 0:
+        print(result.stdout.decode())
+        sys.exit("ADCPOST failed. See output above.")
 
     d1, _ = last_snapshot(os.path.join(testpath , "fort.63.true"))
     d2, _ = last_snapshot(os.path.join(testpath , "fort.63"))
