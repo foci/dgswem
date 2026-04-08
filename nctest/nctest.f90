d61cd85b36197aed7f7af9a04ba9a93c8c5cf7da
= <=>
2026-04-08 16:44:56 -0500

    Fixed typos in nctest

--- nctest/nctest.f90
+++ nctest/nctest.f90
@@ -1,15 +1,15 @@
 program nctest
     
-    use netcdf_fort, only fort_63_nc
+    use netcdf_fort, only : fort_63_nc
     
     implicit none
     
     Type(fort_63_nc) :: my_fort_63_nc
         
     print *, "Creating fort.63.nc"
-    my_fort_63_nc%init("fort.63.nc")
+    call my_fort_63_nc%init("fort.63.nc")
     print *, "Closing fort.63.nc"
-    my_fort_63_nc%close()
+    call my_fort_63_nc%close()
     print *, "Program complete"
     
 end program nctest
