71031fee0f607f4d17ae4d096d339549bd6f04c2
kentonwho <wukenton@gmail.com>
2026-04-27 18:25:26 -0500

    init mpi_aps fixture & apply to quarter_annular

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+import subprocess
+import glob
 
 def dir_path(string):
     if os.path.isdir(string):
@@ -15,3 +17,39 @@ def pytest_addoption(parser):
 def binpath(request):
     p = request.config.getoption("--binpath")
     return dir_path(p)
+
+@pytest.fixture
+def mpi_aps(monkeypatch):
+    ''' Provides a fixture to quickly setup profiling any MPI execution with Intel APS.'''
+    try:
+        subprocess.run(["vtune", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        raise ValueError("Intel APS is part of Intel Vtune. Please install or find Vtune to use this feature.")
+        
+    print('Intercepting subprocess calls with aps wrapper...')
+    original_run = subprocess.run
+    commands_executed = []
+
+    def wrapped_run(*args, **kwargs):
+        cmd = args[0] if args else kwargs.get('args')
+        if cmd and isinstance(cmd, str) and 'mpirun' in cmd:
+            print("Execution command intercepted.")
+            print(f"Intercepted command: {cmd}, with arguments: {args[1:] if len(args) > 1 else ''}, kwargs: {kwargs}")
+            print("Injecting Intel APS wrapper...")
+
+            result = original_run('aps ' + cmd, *args[1:], **kwargs)
+            if result.returncode != 0:
+                print(f"Error executing command: {result.stderr.decode()}")
+            else:
+                working_dir = kwargs.get('cwd')
+                aps_reports = glob.glob(os.path.join(working_dir, 'aps_report*'))
+                newest_report = max(aps_reports, key=os.path.getctime)
+                result = subprocess.run('aps --report .', check=True, cwd=newest_report, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            result = original_run(*args, **kwargs)
+        return result
+
+    monkeypatch.setattr(subprocess, 'run', wrapped_run)
+
+    yield
+
