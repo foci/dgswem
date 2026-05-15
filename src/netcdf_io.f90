48062d9bd8fad49e0f0ff4bd49e04a18bfd48d42
= <=>
2026-05-15 18:34:19 -0500

    Incorporated fort.64 into DGSWEM, but does not render in QGIS

--- src/netcdf_io.f90
+++ src/netcdf_io.f90
@@ -3,12 +3,13 @@ module netcdf_io
 
     !use dg, only : none
     use netcdf, only : nf90_unlimited
-    use ncdg, only : ncdg_63
+    use ncdg, only : ncdg_63, ncdg_64
     
     implicit none
 
     ! Committing the sin of global variables, for now
     type(ncdg_63) :: my_63
+    type(ncdg_64) :: my_64
     
     contains
     
@@ -33,7 +34,8 @@ module netcdf_io
 
         ibtypee = 0 ! This is valid syntax for array of 0s
         ibtype = segtype
-        
+
+        ! fort.63.nc
         call my_63%init("fort.63.nc")
         call my_63%ncdg_63_set_metadata( &
             time_dimsize = nf90_unlimited, &
@@ -70,6 +72,43 @@ module netcdf_io
             nbvv = nbvv &
         )
 
+        ! fort.64.nc
+        call my_64%init("fort.64.nc")
+        call my_64%ncdg_64_set_metadata( &
+            time_dimsize = nf90_unlimited, &
+            node_dimsize = np, &
+            nele_dimsize = ne, &
+            nvertex_dimsize = 3, &
+            nope_dimsize = nope, &
+            neta_dimsize = neta, &
+            max_nvdll_dimsize = maxval(nvdll), &
+            nbou_dimsize = nbou, &
+            nvel_dimsize = nvel, &
+            max_nvell_dimsize = maxval(nvell), &
+            mesh_dimsize = 1, &
+            nope = nope, &
+            nbou = nbou, &
+            ics = ics &
+        )
+        call my_64%ncdg_nodal_write_mesh( &
+            x = x, &
+            y = y, &
+            dp = dp, &
+            nm = nm, &
+            nhy = nhy, &
+            ne = ne, &
+            nope = nope, &
+            neta = neta, &
+            ibtypee = ibtypee, &
+            nvdll = nvdll, &
+            nbdv = nbdv, &
+            nbou = nbou, &
+            nvel = nvel, &
+            ibtype = ibtype, &
+            nvell = nvell, &
+            nbvv = nbvv &
+        )
+
         deallocate(ibtypee)
         deallocate(ibtype)
     end subroutine netcdf_create_files_serial
@@ -77,17 +116,19 @@ module netcdf_io
     subroutine netcdf_write_step_serial()
         !! Write current state to all NetCDF output files for the simulation.
 
-        use global, only : eta2, time_a
+        use global, only : eta2, time_a, uu2, vv2
 
         implicit none
         
         call my_63%ncdg_63_write_step(t=time_a, zeta=eta2)
+        call my_64%ncdg_64_write_step(t=time_a, u_vel=uu2, v_vel=vv2)
     end subroutine netcdf_write_step_serial
 
     subroutine netcdf_close_files_serial()
         !! Close all NetCDF output files
         
         call my_63%close()
+        call my_64%close()
     end subroutine netcdf_close_files_serial
 
 end module netcdf_io
